ffc4138e7e9ab9cf9a3ce4fbb40b5e2f497f3d46
Nhân Châu KP <ctnhan044@gmail.com>
2024-09-15 15:48:00 +0700

    upload sao ke + css

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -6,7 +6,9 @@
             <li><a class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500" target="_blank" href="https://www.facebook.com/mttqvietnam/posts/pfbid0YSaZTjEw2GBMnT5bNBi49djNxnxi326VjKodHzdxvhpkW3rwTs8u5dCeVGvQmU18l">Số tiền ủng hộ qua số tài khoản Vietcombank 0011001932418 từ ngày 1/9/2024 đến ngày 10/9/2024</a></li>
             <li><a class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500" target="_blank" href="https://www.facebook.com/mttqvietnam/posts/pfbid0cGpdUA8JFuB5TPLFX5E1GmhBSkYs99v9rRLY5sY2mTD1pJ16Cq1BvtkrSXLfnsESl">Số tiền ủng hộ qua số tài khoản Vietinbank CT1111 từ ngày 10/9/2024 đến ngày 12/9/2024</a></li>
             <li><a class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500" target="_blank" href="https://www.facebook.com/mttqvietnam/posts/pfbid02Xd59uafubKYurxCtfuoscoRWQosyc6X9VbZaG88DgoxWiHYsrmyz44Tq6GBgJm8fl">Số tiền ủng hộ qua ngân hàng BIDV số tài khoản 1261122666 từ ngày 1/9/2024 đến ngày 12/9/2024</a></li>
+            <li><a class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500" href="https://www.fshare.vn/folder/HVNBBFX5K6WT" target="_blank">Tải PDF sao kê</a></li>
         </ul>
+
     </div>
     <form wire:submit.prevent="searchAction" class="mt-4">
         <div class="relative">
